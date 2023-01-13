647642a7fddbae69bf8330f87a8fd557b19e5b9e
dolev31 <idolevi3197@gmail.com>
2023-01-13 17:18:27 +0200

    roberta works

--- create_split.py
+++ create_split.py
@@ -3,13 +3,23 @@ import torch.optim as optim
 from torch.optim.lr_scheduler import CosineAnnealingLR
 import torch.cuda.amp as amp
 from torch.utils.data import TensorDataset, DataLoader
+from sentence_transformers import SentenceTransformer, util
 import numpy as np
+import torch
+import os
+import torch.nn as nn
+from sklearn.metrics import classification_report
+from tqdm import tqdm
+
+SPAM_COLLECTION = './SMSSpamCollection.txt'
+TARGET_NAMES = ['Ham', 'Spam']
 
 
 def split_collection():
+    print("Processing the spam collection data...")
     spams = []
     hams = []
-    with open(r"C:\Users\noysc\Desktop\SMSSpamCollection.txt", encoding="utf8") as f:
+    with open(SPAM_COLLECTION, encoding="utf8") as f:
         for line in f.readlines():
             words = line.split("	")
             if words[0] == "ham":
@@ -19,67 +29,98 @@ def split_collection():
     return hams, spams
 
 
-# with open(r"C:\Users\noysc\Desktop\spam.txt", "w", encoding="utf8") as f:
-# 	f.writelines(spams)
-# with open(r"C:\Users\noysc\Desktop\hams.txt", "w", encoding="utf8") as f:
-# 	f.writelines(hams)
-
-
-def roberta_classifier(spam, ham, batch_size=256, num_epochs=5000):
+def roberta_classifier(spam, ham, batch_size=60, num_epochs=5000):
     # Set the device and set the seed value for reproducibility
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     torch.manual_seed(42)
 
     # Load the RoBerta model
-    model = transformers.RobertaModel.from_pretrained('roberta-base').cuda()
+    if os.path.isfile('./roberta_clean'):
+        model = transformers.RobertaForSequenceClassification.from_pretrained('./roberta_clean').cuda()
+    else:
+        model = transformers.RobertaForSequenceClassification.from_pretrained('roberta-base').cuda()
+        model.save_pretrained('roberta_clean')
 
+    for param in model.roberta.parameters():
+        param.requires_grad = False
     # Tokenize the sentences using the RoBERTa tokenizer
     tokenizer = transformers.RobertaTokenizer.from_pretrained('roberta-base')
 
-    ham_inputs = tokenizer(ham, return_tensors='pt', padding=True, truncation=True)
-    spam_inputs = tokenizer(spam, return_tensors='pt', padding=True, truncation=True)
+    max_length = max(len(max(ham, key=len)), len(max(spam, key=len)))
+    if max_length > 400:
+        max_length = 400
+
+    ham_inputs = tokenizer.batch_encode_plus(ham, max_length=max_length, truncation=True, pad_to_max_length=True,
+                                             return_tensors='pt')
+    spam_inputs = tokenizer.batch_encode_plus(spam, max_length=max_length, truncation=True, pad_to_max_length=True,
+                                              return_tensors='pt')
 
     # Define the labels for each domain (0 for ham, 1 forspam)
-    ham_labels = torch.tensor([0] * len(ham)).cuda()
-    spam_labels = torch.tensor([1] * len(spam)).cuda()
+    ham_labels = torch.tensor([0] * len(ham)).cuda()[:10]
+    spam_labels = torch.tensor([1] * len(spam)).cuda()[:10]
 
     # Concatenate the inputs and labels for both domains
     inputs = torch.cat([ham_inputs['input_ids'], spam_inputs['input_ids']], dim=0).cuda()
+    attention_mask = torch.cat([ham_inputs['attention_mask'], spam_inputs['attention_mask']], dim=0).cuda()
     labels = torch.cat([ham_labels, spam_labels], dim=0)
 
     # Define a TensorDataset with the inputs and labels
-    dataset = TensorDataset(inputs, labels)
+    dataset = TensorDataset(inputs, attention_mask, labels)
 
     # Create a DataLoader from the dataset
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
     # Set up the optimizer and criterion
-    optimizer = optim.Adam(model.parameters(), lr=0).cuda()
-    scheduler1 = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda x: min(x / 100, 1) * 1e-6)
+    optimizer = optim.Adam(model.classifier.parameters(), lr=0)
+
+    scheduler1 = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda x: min(x / 20, 1) * 1e-6)
+
+    scheduler2 = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10000)
+
     torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=num_epochs * len(dataloader))
-    criterion = nn.CrossEntropyLoss()
+    # criterion = nn.CrossEntropyLoss()
 
     # Fine-tune the model
     model.train()
-    for epoch in range(num_epochs):
+    loss_lst = []
+    print("Start training RoBerta spam classifier...")
+    for epoch in tqdm(range(num_epochs)):
         total_loss = 0
+        epoch_predictions = []
+        epoch_labels = []
         for batch in dataloader:
-            input_ids, attention_mask = batch
-            input_ids, attention_mask = input_ids.to(device), attention_mask.to(device)
+            batch_inputs_ids, batch_attention_mask, batch_labels = batch
             with amp.autocast():
-                output = model(input_ids=input_ids, attention_mask=attention_mask)
-                loss = criterion(output[0], input_ids.argmax(dim=2))
-            total_loss += loss.item()
+                output = model(input_ids=batch_inputs_ids, attention_mask=batch_attention_mask.to(device),
+                               labels=batch_labels.to(device))
+                batch_logits = output['logits']
+                batch_predictions = batch_logits.argmax(dim=1)
+                batch_loss = output[0]
+            total_loss += batch_loss.item()
             with amp.autocast():
-                loss.backward()
+                batch_loss.backward()
+
             optimizer.step()
-            if epoch < 100:
+            if epoch < 20:
                 scheduler1.step()
             else:
                 scheduler2.step()
+
             optimizer.zero_grad()
+            # Tracking the predictions and labels
+            epoch_predictions += batch_predictions.to("cpu")
+            epoch_labels += batch_labels.to("cpu")
+
+        print(classification_report(epoch_labels, epoch_predictions, target_names=TARGET_NAMES))
+        loss_lst.append(total_loss / len(dataloader))
         print(f'Epoch {epoch + 1}, Loss: {total_loss / len(dataloader)}')
+
+    print("Finished training RoBerta spam classifier...")
     return model
+
+
+def polynomial_decay(iteration):
+    return (1 - iteration / 30000) ** 0.9
 
 
 def bart_ham(ham):
@@ -88,25 +129,28 @@ def bart_ham(ham):
     torch.manual_seed(42)
 
     # Load the BART model
-    model = transformers.BartModel.from_pretrained('bart-base')
+    if os.path.isfile('./bart_clean'):
+        model = transformers.BartModel.from_pretrained('./bart_clean').cuda()
+    else:
+        model = transformers.BartModel.from_pretrained('bart-base').cuda()
+        model.save_pretrained('bart_clean')
 
     # Create a dataset for the domain
     dataset = transformers.BartTokenizer.from_pretrained('bart-base').batch_encode_plus(ham, pad_to_max_length=True,
-                                                                                        return_tensors='pt')
+                                                                                        return_tensors='pt',
+                                                                                        truncation=True)
 
     # Create the dataloader
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=3000, shuffle=True)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
 
     # Set up the optimizer and criterion
     optimizer = optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), weight_decay=0.1)
     criterion = nn.CrossEntropyLoss()
 
     # Define the linear warmup schedule
-    scheduler1 = optim.lr_scheduler.LambdaLR(optimizer, lambda x: min(x / 5000, 1) * 3e-5)
+    scheduler1 = optim.lr_scheduler.LambdaLR(optimizer, lambda x: min(x / 20, 1) * 3e-5)
 
     # Define the polynomial decay schedule
-    def polynomial_decay(iteration):
-        return (1 - iteration / 30000) ** 0.9
 
     scheduler2 = optim.lr_scheduler.LambdaLR(optimizer, polynomial_decay)
 
@@ -115,7 +159,8 @@ def bart_ham(ham):
 
     # Fine-tune the model
     model.train()
-    for iteration in range(30000):
+
+    for epoch in tqdm(range(30000)):
         total_loss = 0
         for batch in dataloader:
             input_ids, attention_mask = batch
@@ -127,41 +172,91 @@ def bart_ham(ham):
             with amp.autocast():
                 loss.backward()
             optimizer.step()
-            if iteration < 5000:
+            if epoch < 20:
                 scheduler1.step()
             else:
                 scheduler2.step()
             optimizer.zero_grad()
-        print(f'Iteration {iteration + 1}, Loss: {total_loss / len(dataloader)}')
+        print(f'Iteration {epoch + 1}, Loss: {total_loss / len(dataloader)}')
 
+    print("Finished training RoBerta spam classifier...")
     return model
 
 
-def add_mask(classifier, lm, sentence, k):
-    tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
-    input_ids = torch.tensor(tokenizer.encode(sentence)).unsqueeze(0)
-    outputs = classifier(input_ids)
-    last_hidden_states = outputs[0]
+def add_mask(classifier, lm, sentences, k):
+    tokenizer = transformers.RobertaTokenizer.from_pretrained("roberta-base")
+    fake_sentences = []
+    for sentence in sentences:
+        input_ids = torch.tensor(tokenizer.encode(sentence)).unsqueeze(0)
+        outputs = classifier(input_ids)
+        last_hidden_states = outputs[0]
 
-    # Get the k words with the highest attention
-    indices = np.argsort(last_hidden_states[0], axis=-1)[-k:]
-    for index in indices:
-        input_ids[0][index] = tokenizer.mask_token_id
+        # Get the k words with the highest attention
+        indices = np.argsort(last_hidden_states[0], axis=-1)[-k:]
+        for index in indices:
+            input_ids[0][index] = tokenizer.mask_token_id
 
-    # Generate the completions for the masked tokens
-    new_sentece_ids = lm.generate(input_ids)
-    completions_text = tokenizer.decode(new_sentece_ids[0], skip_special_tokens=True)
-    return completions_text
+        # Generate the completions for the masked tokens
+        new_sentece_ids = lm.generate(input_ids)
+        fake_sentences.append(tokenizer.decode(new_sentece_ids[0], skip_special_tokens=True))
+    return fake_sentences
 
 
-def performance(sentences, fake_sentences):
+def add_msak_baseline(classifier, lm, sentences, k):
     pass
+
+
+def model_performance(sentences, fake_sentences):
+    tokenizer = transformers.RobertaTokenizer.from_pretrained("roberta-base")
+    model = transformers.RobertaForSequenceClassification.from_pretrained(
+        'mariagrandury/roberta-base-finetuned-sms-spam-detection').cuda()
+    input_ids = torch.tensor([tokenizer.encode(sentence, add_special_tokens=True) for sentence in sentences])
+
+    with torch.no_grad():
+        logits = model(input_ids)[0]
+        predictions = torch.argmax(logits, dim=1)
+
+    correct_predictions = (predictions == 1).sum().item()
+
+    # Print the accuracy
+    accuracy = correct_predictions / len(sentences)
+    print("Original Accuracy: {:.2f}%".format(accuracy * 100))
+
+    input_ids = torch.tensor([tokenizer.encode(sentence, add_special_tokens=True) for sentence in fake_sentences])
+
+    with torch.no_grad():
+        logits = model(input_ids)[0]
+        predictions = torch.argmax(logits, dim=1)
+
+    correct_predictions = (predictions == 1).sum().item()
+
+    # Print the accuracy
+    accuracy = correct_predictions / len(sentences)
+    print("Fake Accuracy: {:.2f}%".format(accuracy * 100))
+
+
+def similar_performance(sentences, fake_sentences):
+    model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+    similarity = 0
+    counter = 0
+    for original, fake in zip(sentences, fake_sentences):
+        # Compute embedding for both lists
+        original_embedding = model.encode(original, convert_to_tensor=True)
+        fake_embedding = model.encode(fake, convert_to_tensor=True)
+        similarity += util.pytorch_cos_sim(original_embedding, fake_embedding)
+        counter += 1
+    print(f"Similarity measure: {similarity / counter}")
 
 
 if __name__ == '__main__':
     hams, spams = split_collection()
-    roberta_model = roberta_classifier(spams, hams)
+    # roberta_model = roberta_classifier(spams, hams)
+    # roberta_model.save_pretrained('roberta_trained')
     bart_model = bart_ham(hams)
-    k = 1
-    for sentence in spams:
-        new_sentence = add_mask(roberta_model, bart_model, sentence, k)
+    bart_model.save_pretrained('bart_trained')
+
+    # for k in range(6):
+    #     fake_spams = add_mask(roberta_model, bart_model, spams, k)
+    #     print("Treshold: {k}")
+    #     model_performance(spams, fake_spams)
+    #     similar_performance(spams, fake_spams)
