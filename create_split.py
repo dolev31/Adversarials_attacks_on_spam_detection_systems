14dec7d61f602c45ae49a2291e5471e8a0e34212
dolev31 <idolevi3197@gmail.com>
2023-01-14 20:14:22 +0200

    Everything works

--- create_split.py
+++ create_split.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import transformers
 import torch.optim as optim
 from torch.optim.lr_scheduler import CosineAnnealingLR
@@ -10,6 +11,9 @@ import os
 import torch.nn as nn
 from sklearn.metrics import classification_report
 from tqdm import tqdm
+import random
+import heapq
+from collections import defaultdict
 
 SPAM_COLLECTION = './SMSSpamCollection.txt'
 TARGET_NAMES = ['Ham', 'Spam']
@@ -23,13 +27,13 @@ def split_collection():
         for line in f.readlines():
             words = line.split("	")
             if words[0] == "ham":
-                hams.append(words[1])
+                hams.append(words[1].strip())
             elif words[0] == "spam":
-                spams.append(words[1])
+                spams.append(words[1].strip())
     return hams, spams
 
 
-def roberta_classifier(spam, ham, batch_size=16, num_epochs=60):
+def roberta_classifier(spam, ham, batch_size=16, num_epochs=50):
     # Set the device and set the seed value for reproducibility
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     torch.manual_seed(42)
@@ -71,11 +75,11 @@ def roberta_classifier(spam, ham, batch_size=16, num_epochs=60):
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
     # Set up the optimizer and criterion
-    optimizer = optim.Adam(model.classifier.parameters(), lr=0)
+    optimizer = optim.Adam(model.classifier.parameters(), lr=0.001)
 
-    scheduler1 = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda x: min(x / 20, 1) * 1e-6)
+    # scheduler1 = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda x: min(x / 20, 1) * 1e-6)
 
-    scheduler2 = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10000)
+    # scheduler2 = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10000)
 
     torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=num_epochs * len(dataloader))
     # criterion = nn.CrossEntropyLoss()
@@ -101,10 +105,10 @@ def roberta_classifier(spam, ham, batch_size=16, num_epochs=60):
                 batch_loss.backward()
 
             optimizer.step()
-            if epoch < 20:
-                scheduler1.step()
-            else:
-                scheduler2.step()
+            # if epoch < 20:
+            # scheduler1.step()
+            # else:
+            # scheduler2.step()
 
             optimizer.zero_grad()
             # Tracking the predictions and labels
@@ -130,17 +134,18 @@ def bart_ham(ham):
 
     # Load the BART model
     if os.path.isfile('./bart_clean'):
-        model = transformers.BartModel.from_pretrained('./bart_clean').cuda()
+        model = transformers.BartForConditionalGeneration.from_pretrained("./bart_clean").cuda()
     else:
-        model = transformers.BartModel.from_pretrained("facebook/bart-base").cuda()
-        model.save_pretrained('bart_clean')
+        model = transformers.BartForConditionalGeneration.from_pretrained("facebook/bart-base",
+                                                                          forced_bos_token_id=0).cuda()
+        model.save_pretrained('bart_new')
 
-    tokenizer = transformers.RobertaTokenizer.from_pretrained('facebook/bart-base')
+    tokenizer = transformers.BartTokenizer.from_pretrained("facebook/bart-base")
 
-    # Create a dataset for the domain
+    # Create a dataset for the domain	
     tokenized_data = tokenizer.batch_encode_plus(ham, pad_to_max_length=True,
-                                          return_tensors='pt',
-                                          truncation=True)
+                                                 return_tensors='pt',
+                                                 truncation=True)
 
     inputs = tokenized_data['input_ids'].cuda()
     attention_mask = tokenized_data['attention_mask'].cuda()
@@ -148,18 +153,18 @@ def bart_ham(ham):
     dataset = TensorDataset(inputs, attention_mask)
 
     # Create the dataloader
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=24, shuffle=True)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=4, shuffle=True)
 
     # Set up the optimizer and criterion
-    optimizer = optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), weight_decay=0.1)
+    optimizer = optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.98), weight_decay=0.1)
     criterion = nn.CrossEntropyLoss()
 
     # Define the linear warmup schedule
-    scheduler1 = optim.lr_scheduler.LambdaLR(optimizer, lambda x: min(x / 20, 1) * 3e-5)
+    # scheduler1 = optim.lr_scheduler.LambdaLR(optimizer, lambda x: min(x / 20, 1) * 3e-5)
 
     # Define the polynomial decay schedule
 
-    scheduler2 = optim.lr_scheduler.LambdaLR(optimizer, polynomial_decay)
+    # scheduler2 = optim.lr_scheduler.LambdaLR(optimizer, polynomial_decay)
 
     # Set the dropout
     dropout = 0.3
@@ -168,6 +173,7 @@ def bart_ham(ham):
     model.train()
     print("Start fine tuning Bart model...")
     for epoch in tqdm(range(30)):
+        print(f"\n Currently at epoch {epoch}...")
         total_loss = 0
         for batch in dataloader:
             input_ids, attention_mask = batch
@@ -183,45 +189,199 @@ def bart_ham(ham):
             with amp.autocast():
                 loss.backward()
             optimizer.step()
-            if epoch < 20:
-                scheduler1.step()
-            else:
-                scheduler2.step()
+            # if epoch < 20:
+            #     scheduler1.step()
+            # else:
+            #     scheduler2.step()
             optimizer.zero_grad()
         print(f'Iteration {epoch + 1}, Loss: {total_loss / len(dataloader)}')
-
-    print("Finished training RoBerta spam classifier...")
+        # Creates a checkpoint after each 10 epochs
+        if epoch % 10 == 0:
+            model.save_pretrained('bart_trained')
+    print("Finished training ham Bart...")
     return model
 
 
-def add_mask(classifier, lm, sentences, k):
-    tokenizer = transformers.RobertaTokenizer.from_pretrained("roberta-base")
+def corruption(word):
+    abc = "abcdefghijklmnopqrstuvwxyz"
+    p = random.random()
+    index = random.choice(range(len(word)))
+    if p < 1 / 3:
+        word[index] = ""
+    elif p < 2 / 3:
+        word[index] = random.choice(abc)
+    else:
+        word[index] = word[:index] + random.choice(abc) + word[index:]
+
+
+def add_mask(classifier, lm, sentences, k, choosing_baseline=False, replacing_baseline=False):
+    # roberta_tokenizer = transformers.RobertaTokenizer.from_pretrained("roberta-base")
+    roberta_tokenizer = transformers.RobertaTokenizerFast.from_pretrained("roberta-base")
+    bart_tokenizer = transformers.BartTokenizer.from_pretrained("facebook/bart-large")
     fake_sentences = []
     for sentence in sentences:
-        input_ids = torch.tensor(tokenizer.encode(sentence)).unsqueeze(0)
-        outputs = classifier(input_ids)
-        last_hidden_states = outputs[0]
+        if choosing_baseline:  # choose k words according to the score after deleting the sentence
+            scores = {}
+            list_sentence = sentence.split(" ")
+            for i in range(len(list_sentence)):
+                if i == len(list_sentence) - 1:
+                    new_sentence = " ".join(list_sentence[:-1])
+                else:
+                    new_sentence = " ".join(list_sentence[:i] + list_sentence[i + 1:])
+                input_ids = torch.tensor(roberta_tokenizer.encode(new_sentence)).unsqueeze(0)
+                output = classifier(input_ids)
+                scores[i] = output["logits"][0][1].item()
+            indices = heapq.nsmallest(k, scores, key=scores.get)
+            for index in indices:
+                list_sentence[index] = bart_tokenizer.mask_token
+            masked_sentence = " ".join(list_sentence)
+            input_ids = torch.tensor(bart_tokenizer.encode(masked_sentence)).unsqueeze(0)
+            # Generate the completions for the masked tokens
+            new_sentece_ids = lm.generate(input_ids, max_length=len(input_ids[0]) * 2, num_beams=5)
+            fake_sentences.append(bart_tokenizer.decode(new_sentece_ids[0], skip_special_tokens=True))
 
-        # Get the k words with the highest attention
-        indices = np.argsort(last_hidden_states[0], axis=-1)[-k:]
-        for index in indices:
-            input_ids[0][index] = tokenizer.mask_token_id
+        elif replacing_baseline:  # choose k words according to the attention score and corrupt them
 
-        # Generate the completions for the masked tokens
-        new_sentece_ids = lm.generate(input_ids)
-        fake_sentences.append(tokenizer.decode(new_sentece_ids[0], skip_special_tokens=True))
+            # Tokenize the sentence
+            # input_ids = roberta_tokenizer.encode(sentence, return_tensors='pt')
+            input_ids = roberta_tokenizer(sentence, return_tensors='pt', return_offsets_mapping=True)
+            # Run the sentence in the model inference
+            outputs = classifier(input_ids["input_ids"], output_attentions=True)
+            # Extract the last attention layer
+            last_hidden_states = outputs.attentions[-1][0][0][0]
+
+            # Get the k words with the highest attention
+
+            sentence_lst = sentence.split(" ")
+
+            ##########################################################
+            sentence_dict = {}
+            counter = 0
+            for i, word in enumerate(sentence.split(" ")):
+                sentence_dict[(counter, counter + len(word))] = i
+                counter = counter + len(word) + 1
+            offsets = input_ids["offset_mapping"].squeeze(0)
+            offsets_dicts = {(item[0].item(), item[1].item()): i for i, item in enumerate(offsets)}
+
+            def map_words(dict1, dict2):
+                new_dict = {}
+                for key1, value1 in dict1.items():
+                    words = []
+                    for key2, value2 in dict2.items():
+                        if key1[0] <= key2[0] and key2[1] <= key1[1]:
+                            if key2 == (0, 0):
+                                if -1 in new_dict:
+                                    new_dict[-1].append(value2)
+                                else:
+                                    new_dict[-1] = [value2]
+                                continue
+                            words.append(value2)
+                        if key2[0] > key1[1]:
+                            break
+                    new_dict[value1] = words
+                return new_dict
+
+            holy_dict = map_words(sentence_dict, offsets_dicts)
+            holy_dict[-1].append(0)
+            reversed_dict = {v: k for k, values in holy_dict.items() for v in values}
+
+            # Get the k words with the highest attention
+            attention_order = np.argsort(last_hidden_states.detach(), axis=-1).tolist()
+            mask_indices = set()
+            for i in range(len(attention_order) - 1, -1, -1):
+                if reversed_dict[attention_order[i]] == -1:
+                    continue
+                if len(mask_indices) == k:
+                    break
+                mask_indices.add(reversed_dict[attention_order[i]])
+                counter += 1
+
+            ##########################################################
+
+            for index in mask_indices:
+                sentence_lst[index] = corruption(sentence_lst[index])
+
+            fake_sentences.append(" ".join(sentence_lst))
+        else:
+            # Tokenize the sentence
+            # input_ids = roberta_tokenizer.encode(sentence, return_tensors='pt')
+            input_ids = roberta_tokenizer(sentence, return_tensors='pt', return_offsets_mapping=True)
+            # Run the sentence in the model inference
+            outputs = classifier(input_ids["input_ids"], output_attentions=True)
+            # Extract the last attention layer
+            last_hidden_states = outputs.attentions[-1][0][0][0]
+
+            # Get the k words with the highest attention
+
+            sentence_lst = sentence.split(" ")
+
+            ##########################################################
+            sentence_dict = {}
+            counter = 0
+            for i, word in enumerate(sentence.split(" ")):
+                sentence_dict[(counter, counter + len(word))] = i
+                counter = counter + len(word) + 1
+            offsets = input_ids["offset_mapping"].squeeze(0)
+            offsets_dicts = {(item[0].item(), item[1].item()): i for i, item in enumerate(offsets)}
+
+            def map_words(dict1, dict2):
+                new_dict = {}
+                for key1, value1 in dict1.items():
+                    words = []
+                    for key2, value2 in dict2.items():
+                        if key1[0] <= key2[0] and key2[1] <= key1[1]:
+                            if key2 == (0, 0):
+                                if -1 in new_dict:
+                                    new_dict[-1].append(value2)
+                                else:
+                                    new_dict[-1] = [value2]
+                                continue
+                            words.append(value2)
+                        if key2[0] > key1[1]:
+                            break
+                    new_dict[value1] = words
+                return new_dict
+
+            holy_dict = map_words(sentence_dict, offsets_dicts)
+            holy_dict[-1].append(0)
+            reversed_dict = {v: k for k, values in holy_dict.items() for v in values}
+
+            # Get the k words with the highest attention
+            attention_order = np.argsort(last_hidden_states.detach(), axis=-1).tolist()
+            mask_indices = set()
+            for i in range(len(attention_order) - 1, -1, -1):
+                if reversed_dict[attention_order[i]] == -1:
+                    continue
+                if len(mask_indices) == k:
+                    break
+                mask_indices.add(reversed_dict[attention_order[i]])
+                counter += 1
+
+            ##########################################################
+
+            for index in mask_indices:
+                sentence_lst[index] = bart_tokenizer.mask_token
+
+            masked_sentence = " ".join(sentence_lst)
+            input_ids = torch.tensor(bart_tokenizer.encode(masked_sentence)).unsqueeze(0)
+
+            # Generate the completions for the masked tokens
+            new_sentence_ids = lm.generate(input_ids, max_length=len(input_ids[0]) * 2, num_beams=5)
+            fake_sentences.append(bart_tokenizer.decode(new_sentence_ids[0], skip_special_tokens=True))
+
     return fake_sentences
 
 
-def add_msak_baseline(classifier, lm, sentences, k):
-    pass
+def model_performance(sentences, fake_sentences, k):
+    sentences_df = pd.DataFrame({"original_sentences": sentences, "adversarial_sentences": fake_sentences})
+    sentences_df.to_csv(f"sentences_{k}")
 
-
-def model_performance(sentences, fake_sentences):
     tokenizer = transformers.RobertaTokenizer.from_pretrained("roberta-base")
     model = transformers.RobertaForSequenceClassification.from_pretrained(
-        'mariagrandury/roberta-base-finetuned-sms-spam-detection').cuda()
-    input_ids = torch.tensor([tokenizer.encode(sentence, add_special_tokens=True) for sentence in sentences])
+        'mariagrandury/roberta-base-finetuned-sms-spam-detection')
+    input_ids = torch.tensor(
+        [tokenizer.encode(sentence, add_special_tokens=True, pad_to_max_length=True, max_length=200, truncation=True)
+         for sentence in sentences])
 
     with torch.no_grad():
         logits = model(input_ids)[0]
@@ -233,10 +393,12 @@ def model_performance(sentences, fake_sentences):
     accuracy = correct_predictions / len(sentences)
     print("Original Accuracy: {:.2f}%".format(accuracy * 100))
 
-    input_ids = torch.tensor([tokenizer.encode(sentence, add_special_tokens=True) for sentence in fake_sentences])
+    input_ids = torch.tensor(
+        [tokenizer.encode(sentence, add_special_tokens=True, pad_to_max_length=True, max_length=200, truncation=True)
+         for sentence in fake_sentences])
 
     with torch.no_grad():
-        logits = model(input_ids)[0]
+        logits = model(input_ids)["logits"]
         predictions = torch.argmax(logits, dim=1)
 
     correct_predictions = (predictions == 1).sum().item()
@@ -263,11 +425,17 @@ if __name__ == '__main__':
     hams, spams = split_collection()
     # roberta_model = roberta_classifier(spams, hams)
     # roberta_model.save_pretrained('roberta_trained')
+
+    # Train Bart
     bart_model = bart_ham(hams)
     bart_model.save_pretrained('bart_trained')
 
-    # for k in range(6):
+    # Create adversarial attacks
+    # roberta_model = transformers.RobertaForSequenceClassification.from_pretrained('./roberta_clean')
+    # # roberta_model = transformers.RobertaModel.from_pretrained('./roberta_clean')
+    # bart_model = transformers.BartForConditionalGeneration.from_pretrained("./bart_clean")
+    # for k in range(1, 5):
     #     fake_spams = add_mask(roberta_model, bart_model, spams, k)
     #     print("Treshold: {k}")
-    #     model_performance(spams, fake_spams)
+    #     model_performance(spams, fake_spams, k)
     #     similar_performance(spams, fake_spams)
